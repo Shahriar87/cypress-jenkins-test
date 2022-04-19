da97786d0f24ffb460945d6948498a71a0d62e1d
Shahriar Firoz <shahriar.firoz@gmail.com>
2022-04-18 21:57:01 -0500

    port change for cypress

--- cypress/integration/heroes_list_spec.js
+++ cypress/integration/heroes_list_spec.js
@@ -1,20 +1,18 @@
-describe('Testing the hero list page', function() {
+describe("Testing the hero list page", function () {
+  it("should have heroes in the list", function () {
+    cy.visit("http://localhost:4201/heroes");
 
-  it('should have heroes in the list', function() {
-    cy.visit('http://localhost:4200/heroes');
-
-    const heroesListElements = cy.get('ul.heroes > li');
-    heroesListElements.should('have.length', 10);
+    const heroesListElements = cy.get("ul.heroes > li");
+    heroesListElements.should("have.length", 10);
   });
 
-  it('should add a new hero to the list when creating a new hero', function() {
-    cy.visit('http://localhost:4200/heroes');
+  it("should add a new hero to the list when creating a new hero", function () {
+    cy.visit("http://localhost:4201/heroes");
 
-    cy.get('input').type("Dr. Angular");
-    cy.get('button').click();
+    cy.get("input").type("Dr. Angular");
+    cy.get("button").click();
 
-    const heroesListElements = cy.get('ul.heroes > li');
-    heroesListElements.should('have.length', 11);
+    const heroesListElements = cy.get("ul.heroes > li");
+    heroesListElements.should("have.length", 11);
   });
-
 });
